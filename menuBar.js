d5e967a6a445998041e88101150e83f0469e16e5
Felipe Esaki Roman <120192433+FelipeEsaki@users.noreply.github.com>
2023-09-14 15:26:22 -0300

    Commit 3 - bug fix Home mobile

--- menuBar.js
+++ menuBar.js
@@ -4,7 +4,7 @@ const btnFechar = document.getElementById('close-btn');
 
 menuIcon.addEventListener('click', () => {
     menuBar.style.top = '0';
-    menuBar.style.transition = '.3s';
+    menuBar.style.transition = '.5s';
 });
 
 btnFechar.addEventListener('click', () => {
